83f5154f0dfbec17c6df9fec4047832700e80f3a
MatheusBaarros <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>
2025-11-10 12:26:05 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+function criarTarefa(textoDaTarefa) {
+const li = document.createElement('li');{
+
+
+    
+
+
+}
+
+
+}
